51b58c473ba8e97ec84cf1c5fb7e686990e6b368
Addy <addywchi@gmail.com>
2017-09-22 11:38:34 +0800

    Update assembly information

--- ConsoleApp/DetectCircularLinkList/Properties/AssemblyInfo.cs
+++ ConsoleApp/DetectCircularLinkList/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DetectCircularLinkList")]
+[assembly: AssemblyTitle("ThirtyMinutes.ConsoleApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DetectCircularLinkList")]
+[assembly: AssemblyProduct("Detect Circular Link List")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
